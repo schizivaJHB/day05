fa202fc5ef1c40a1262764dcc1e6bb2820dd2e8f
Sharai Chizivano <schiziva@c4r10s5.wethinkcode.co.za>
2020-06-25 15:55:48 +0200

    ex03 complete

---
+++ ex03/ft_strcmp.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: schiziva <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/25 15:54:06 by schiziva          #+#    #+#             */
+/*   Updated: 2020/06/25 15:55:03 by schiziva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+char *ft_strncpy(char *dest, char *arc, unsigned int n)
+{
+  int n;
+  n = 0;
+
+  while(arc1[n] != '\0')
+  {
+	  dest1[n] = arch1[n];
+	  ft_putchar(dest1[n]);
+	  n = n+1;
+  }
+  return(dest);
+}
+
+int main()
+{
+	char arch1[] = {"let us go"}
+	char dest1[sizeof (arch1)];
+
+	ft_strcpy(dest1, arch1);
+}
