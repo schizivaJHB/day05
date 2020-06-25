62ef36d853433c6af2755238d4e6997bf24ae85b
Sharai Chizivano <schiziva@c4r10s5.wethinkcode.co.za>
2020-06-25 15:52:12 +0200

    ex02 complete

---
+++ ex02/ft_strstr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: schiziva <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/25 15:46:41 by schiziva          #+#    #+#             */
+/*   Updated: 2020/06/25 15:49:06 by schiziva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+char *ft_strncpy(char *dest, char *arc, unsigned int n)
+{
+  int n;
+  n = 0;
+
+  while(arc1[n] != '\0')
+  {
+	  dest1[n] = arch1[n];
+	  ft_putchar(dest1[n]);
+	  n = n+1;
+  }
+  return(dest);
+}
+
+int main()
+{
+	char arch1[] = {"let us go"}
+	char dest1[sizeof (arch1)];
+
+	ft_strcpy(dest1, arch1);
+}
